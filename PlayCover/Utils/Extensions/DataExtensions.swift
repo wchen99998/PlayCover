7ea80ef45fab642f6a5116a57fb7a285e991cd77
Wccccc <43540548+cjim8889@users.noreply.github.com>
2026-03-27 23:19:15 +0800

    Pin inject fix and harden Mach-O string parsing

--- PlayCover/Utils/Extensions/DataExtensions.swift
+++ PlayCover/Utils/Extensions/DataExtensions.swift
@@ -10,9 +10,19 @@ extension String {
     init(data: Data, offset: Int, commandSize: Int, loadCommandString: lc_str) {
         let loadCommandStringOffset = Int(loadCommandString.offset)
         let stringOffset = offset + loadCommandStringOffset
-        let length = commandSize - loadCommandStringOffset
-        self = String(data: data[stringOffset..<(stringOffset + length)],
-                      encoding: .utf8)!.trimmingCharacters(in: .controlCharacters)
+        let commandEnd = offset + commandSize
+
+        guard loadCommandStringOffset > 0,
+              stringOffset >= data.startIndex,
+              stringOffset <= commandEnd,
+              commandEnd <= data.endIndex else {
+            self = ""
+            return
+        }
+
+        let rawStringData = data[stringOffset..<commandEnd].prefix { $0 != 0 }
+        self = String(decoding: rawStringData, as: UTF8.self)
+            .trimmingCharacters(in: .controlCharacters)
     }
 }
 
